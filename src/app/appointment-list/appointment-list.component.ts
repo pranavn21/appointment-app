0e378ea58b06b66932eb36df82dc0f516ce8fda0
pranavn21 <pxn190027@utdallas.edu>
2025-02-23 00:27:51 -0600

    Completed initial data binding and initial components w/ bootstrap

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { Appointment } from '../models/appointment';
+import {Appointment} from '../models/appointment'
+import { OnInit } from '@angular/core';
 
-export class AppointmentListComponent {
+@Component({
+  selector: 'app-appointment-list',
+  standalone: false,
+  templateUrl: './appointment-list.component.html',
+  styleUrl: './appointment-list.component.css'
+})
+export class AppointmentListComponent implements OnInit {
 
   newAppointmentTitle : string = "";
   newAppointmentDate : Date = new Date();
 
   appointments: Appointment[] = []
+
+  ngOnInit(): void {
+    // Loading from local storage
+    let savedAppointments = localStorage.getItem("appointments")
+
+    // Check if there's a value in savedAppointments -> if there is (?), parse the data, if not (:) return an empty array.
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : []
+  }
 
   addAppointment(){
     if(this.newAppointmentTitle.trim().length && this.newAppointmentDate){
@@ -16,20 +31,22 @@ export class AppointmentListComponent {
         date: this.newAppointmentDate
       }
 
+      // Pushing info into array
       this.appointments.push(newAppointment)
+
+      // Values are reset & updated
+      this.newAppointmentTitle = "";
       this.newAppointmentDate = new Date();
 
-      alert(this.appointments.length)
+      // Saving data into local storage as a JSON string
+      localStorage.setItem("appointments", JSON.stringify(this.appointments))
     }
-
   }
-    
+
+  // Removing something from a given index in an array
   deleteAppointment(index: number){
     this.appointments.splice(index, 1)
+    localStorage.setItem("appointments", JSON.stringify(this.appointments))
   }
-    // {
-    //   id: 1,
-    //   title: "Take dog for a walk",
-    //   date: new Date('2023-07-30')
-    // }
+
 }
